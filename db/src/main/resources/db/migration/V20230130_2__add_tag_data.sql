d9a9cc2622d4933402f15fb840169fdcf9faa146
neroHua <2676337735@qq.com>
2023-01-30 21:29:03 +0800

    add tag data sql

---
+++ db/src/main/resources/db/migration/V20230130_2__add_tag_data.sql
@@ -0,0 +1,20 @@
+INSERT INTO tag (id, create_time, update_time, create_user_id, update_user_id, code, name, description, parent_id)
+VALUES (1, NOW(), NOW(), 1, 1, 'food', '食物', '食物', null);
+
+INSERT INTO tag (id, create_time, update_time, create_user_id, update_user_id, code, name, description, parent_id)
+VALUES (2, NOW(), NOW(), 1, 1, 'entertainment', '娱乐', '娱乐', null);
+
+INSERT INTO tag (id, create_time, update_time, create_user_id, update_user_id, code, name, description, parent_id)
+VALUES (3, NOW(), NOW(), 1, 1, 'clothing', '衣物', '衣物', null);
+
+INSERT INTO tag (id, create_time, update_time, create_user_id, update_user_id, code, name, description, parent_id)
+VALUES (4, NOW(), NOW(), 1, 1, 'live', '居住', '居住', null);
+
+INSERT INTO tag (id, create_time, update_time, create_user_id, update_user_id, code, name, description, parent_id)
+VALUES (5, NOW(), NOW(), 1, 1, 'traffic', '交通', '交通', null);
+
+INSERT INTO tag (id, create_time, update_time, create_user_id, update_user_id, code, name, description, parent_id)
+VALUES (6, NOW(), NOW(), 1, 1, 'medicalTreatment', '医疗', '医疗', null);
+
+INSERT INTO tag (id, create_time, update_time, create_user_id, update_user_id, code, name, description, parent_id)
+VALUES (7, NOW(), NOW(), 1, 1, 'social', '社交', '社交', null);
