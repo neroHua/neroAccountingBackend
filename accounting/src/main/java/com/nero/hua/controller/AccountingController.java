26bf3be36251b0ce29d0aaa6d1e7d9e3cacf6cde
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-02 18:38:47 +0800

    Create AccountingController.java

---
+++ accounting/src/main/java/com/nero/hua/controller/AccountingController.java
@@ -0,0 +1,32 @@
+package com.nero.hua.controller;
+
+import com.nero.hua.model.base.BaseResponse;
+import com.nero.hua.model.tag.TagAddRequest;
+import com.nero.hua.model.tag.TagResponse;
+import com.nero.hua.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("accounting")
+public class AccountingController {
+
+    @Autowired
+    TagService tagService;
+
+    @PostMapping
+    public BaseResponse<Long> add(@RequestBody @Validated TagAddRequest tagAddRequest) {
+        Long id = tagService.add(tagAddRequest);
+
+        return new BaseResponse<>(id);
+    }
+
+    @GetMapping(value = "/detail/{id}")
+    public BaseResponse<TagResponse> get(@PathVariable(name = "id") Long id) {
+        TagResponse tagResponse = tagService.get(id);
+
+        return new BaseResponse<>(tagResponse);
+    }
+
+}
