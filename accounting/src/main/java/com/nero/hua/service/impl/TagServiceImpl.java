703ab783fd4cd11cc8223f3033593c718e75c42f
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-02 18:41:13 +0800

    Create TagServiceImpl.java

---
+++ accounting/src/main/java/com/nero/hua/service/impl/TagServiceImpl.java
@@ -0,0 +1,28 @@
+package com.nero.hua.service.impl;
+
+import com.nero.hua.bean.TagDO;
+import com.nero.hua.convert.TagConvert;
+import com.nero.hua.dao.TagDAO;
+import com.nero.hua.model.tag.TagAddRequest;
+import com.nero.hua.model.tag.TagResponse;
+import com.nero.hua.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TagServiceImpl implements TagService {
+
+    @Autowired
+    TagDAO tagDAO;
+
+    @Override
+    public Long add(TagAddRequest tagAddRequest) {
+        TagDO tagDO = TagConvert.convertRequestToDO(tagAddRequest);
+        return tagDAO.insertTag(tagDO);
+    }
+
+    @Override
+    public TagResponse get(Long id) {
+        TagDO tagDO = tagDAO.selectById(id);
+        return TagConvert.convertDOToResponse(tagDO);
+    }
+
+}
