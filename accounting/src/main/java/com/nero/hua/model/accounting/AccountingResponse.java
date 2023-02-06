0787adca2f92ca1525e44bbf8b737aeed1bf1892
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-06 17:19:21 +0800

    Update AccountingResponse.java

--- accounting/src/main/java/com/nero/hua/model/accounting/AccountingResponse.java
+++ accounting/src/main/java/com/nero/hua/model/accounting/AccountingResponse.java
@@ -1,4 +1,33 @@
 package com.nero.hua.model.accounting;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
 public class AccountingResponse {
+    private Long id;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Long createUserId;
+
+    private Long updateUserId;
+
+    private Long userId;
+
+    private Date billCreateTime;
+
+    private Double billMoney;
+
+    private String description;
+
+    private String reviewDescription;
+
+    private Boolean valuable;
+
 }
