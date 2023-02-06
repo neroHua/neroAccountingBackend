52d3280909895abb71701ec3ecd9bd1ee4b7692d
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-06 11:05:44 +0800

    Update TagConvert.java

--- accounting/src/main/java/com/nero/hua/convert/TagConvert.java
+++ accounting/src/main/java/com/nero/hua/convert/TagConvert.java
@@ -3,16 +3,56 @@ package com.nero.hua.convert;
 import com.nero.hua.bean.TagDO;
 import com.nero.hua.model.tag.TagAddRequest;
 import com.nero.hua.model.tag.TagResponse;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class TagConvert {
 
-    public static TagDO convertRequestToDO(TagAddRequest tagAddRequest) {
+    public static TagDO convertRequestToDO(TagAddRequest tagAddRequest, Long userId) {
         TagDO tagDO = new TagDO();
+
+        tagDO.setCreateUserId(userId);
+        tagDO.setUpdateUserId(userId);
+        tagDO.setCreateTime(new Date());
+        tagDO.setUpdateTime(new Date());
+
+        tagDO.setCode(tagAddRequest.getCode());
+        tagDO.setName(tagAddRequest.getName());
+        tagDO.setDescription(tagAddRequest.getDescription());
+        tagDO.setParentId(tagAddRequest.getParentId());
+
         return tagDO;
     }
 
     public static TagResponse convertDOToResponse(TagDO tagDO) {
         TagResponse tagResponse = new TagResponse();
+
+        tagResponse.setCreateUserId(tagDO.getCreateUserId());
+        tagResponse.setUpdateUserId(tagDO.getUpdateUserId());
+        tagResponse.setCreateTime(tagDO.getCreateTime());
+        tagResponse.setUpdateTime(tagDO.getUpdateTime());
+
+        tagResponse.setCode(tagDO.getCode());
+        tagResponse.setName(tagDO.getName());
+        tagResponse.setDescription(tagDO.getDescription());
+        tagResponse.setParentId(tagDO.getParentId());
+
         return tagResponse;
+    }
+
+    public static List<TagResponse> convertDOToResponse(List<TagDO> tagDOList) {
+        if (CollectionUtils.isEmpty(tagDOList)) {
+            return null;
+        }
+
+        List<TagResponse> tagResponseList = new ArrayList<>();
+        for (TagDO tagDO : tagDOList) {
+            tagResponseList.add(convertDOToResponse(tagDO));
+        }
+
+        return tagResponseList;
     }
 }
