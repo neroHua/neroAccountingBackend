fdce6604802ce034afa0d018d01333efee1abeb1
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-06 11:04:06 +0800

    Update TagDAO.java

--- accounting/src/main/java/com/nero/hua/dao/TagDAO.java
+++ accounting/src/main/java/com/nero/hua/dao/TagDAO.java
@@ -1,10 +1,17 @@
 package com.nero.hua.dao;
 
 import com.nero.hua.bean.TagDO;
+import com.nero.hua.model.tag.TagPageRequest;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface TagDAO {
     Long insertTag(@Param("tagDO") TagDO tagDO);
 
     TagDO selectById(@Param("id") Long id);
+
+    Long selectCountByPage(@Param("tagPageRequest") TagPageRequest tagPageRequest);
+
+    List<TagDO> selectListByPage(@Param("tagPageRequest") TagPageRequest tagPageRequest, @Param("begin") Long calculateBegin);
 }
