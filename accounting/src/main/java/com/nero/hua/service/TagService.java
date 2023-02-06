2e757013191f6fe1b68563bce475021fe8b29df8
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-06 11:02:26 +0800

    Update TagService.java

--- accounting/src/main/java/com/nero/hua/service/TagService.java
+++ accounting/src/main/java/com/nero/hua/service/TagService.java
@@ -1,44 +1,15 @@
-package com.nero.hua.controller;
+package com.nero.hua.service;
 
 import com.nero.hua.model.base.BasePageResponse;
-import com.nero.hua.model.base.BaseResponse;
 import com.nero.hua.model.tag.TagAddRequest;
 import com.nero.hua.model.tag.TagPageRequest;
 import com.nero.hua.model.tag.TagResponse;
-import com.nero.hua.service.TagService;
-import com.nero.hua.util.LoginUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+public interface TagService {
 
-@RestController
-@RequestMapping("tag")
-public class TagController {
+    Long add(TagAddRequest tagAddRequest, String userId);
 
-    @Autowired
-    TagService tagService;
+    TagResponse get(Long id);
 
-    @PostMapping
-    public BaseResponse<Long> add(@RequestBody @Validated TagAddRequest tagAddRequest, HttpServletRequest httpServletRequest) {
-        String userId = LoginUtil.parseUserIdFromHttpServletRequest(httpServletRequest);
-        Long id = tagService.add(tagAddRequest, userId);
-
-        return new BaseResponse<>(id);
-    }
-
-    @GetMapping(value = "/detail/{id}")
-    public BaseResponse<TagResponse> get(@PathVariable(name = "id") Long id) {
-        TagResponse tagResponse = tagService.get(id);
-
-        return new BaseResponse<>(tagResponse);
-    }
-
-    @GetMapping(value = "/list")
-    public BaseResponse<BasePageResponse<TagResponse>> selectTagByPage(@Validated TagPageRequest tagPageRequest) {
-        BasePageResponse<TagResponse> tagPageResponse = tagService.selectByPage(tagPageRequest);
-        return new BaseResponse(tagPageResponse);
-    }
-
+    BasePageResponse<TagResponse> selectByPage(TagPageRequest tagPageRequest);
 }
