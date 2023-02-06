9dfa2d830b683cfa90e8c8aa6e9e0a79064a9091
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-06 17:20:42 +0800

    Update AccountingDAO.java

--- accounting/src/main/java/com/nero/hua/dao/AccountingDAO.java
+++ accounting/src/main/java/com/nero/hua/dao/AccountingDAO.java
@@ -1,10 +1,17 @@
 package com.nero.hua.dao;
 
 import com.nero.hua.bean.AccountingDO;
+import com.nero.hua.model.accounting.AccountingPageRequest;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface AccountingDAO {
     Long insertAccounting(@Param("accountingDO") AccountingDO accountingDO);
 
     AccountingDO selectById(@Param("id") Long id);
+
+    Long selectCountByPage(@Param("accountingPageRequest") AccountingPageRequest accountingPageRequest);
+
+    List<AccountingDO> selectListByPage(@Param("accountingPageRequest") AccountingPageRequest accountingPageRequest, @Param("begin") Long calculateBegin);
 }
