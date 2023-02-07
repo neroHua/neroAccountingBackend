097889bed88274f1fd2d2f8e42cd26b810256e1e
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-07 14:53:00 +0800

    Update TagService.java

--- accounting/src/main/java/com/nero/hua/service/TagService.java
+++ accounting/src/main/java/com/nero/hua/service/TagService.java
@@ -4,14 +4,18 @@ import com.nero.hua.model.base.BasePageResponse;
 import com.nero.hua.model.tag.TagAddRequest;
 import com.nero.hua.model.tag.TagPageRequest;
 import com.nero.hua.model.tag.TagResponse;
+import com.nero.hua.model.tag.TagUpdateRequest;
 
 public interface TagService {
 
     Long add(TagAddRequest tagAddRequest, String userId);
 
-    void deleteById(Long id);
+    long deleteById(Long id);
+
+    long updateById(TagUpdateRequest tagUpdateRequest, String userId);
 
     TagResponse get(Long id);
 
     BasePageResponse<TagResponse> selectByPage(TagPageRequest tagPageRequest);
+
 }
