bb20439962592adbf15068baa506a6fe2692ec7f
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-07 15:00:07 +0800

    Update TagConvert.java

--- accounting/src/main/java/com/nero/hua/convert/TagConvert.java
+++ accounting/src/main/java/com/nero/hua/convert/TagConvert.java
@@ -3,6 +3,7 @@ package com.nero.hua.convert;
 import com.nero.hua.bean.TagDO;
 import com.nero.hua.model.tag.TagAddRequest;
 import com.nero.hua.model.tag.TagResponse;
+import com.nero.hua.model.tag.TagUpdateRequest;
 import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
@@ -23,6 +24,21 @@ public class TagConvert {
         tagDO.setName(tagAddRequest.getName());
         tagDO.setDescription(tagAddRequest.getDescription());
         tagDO.setParentId(tagAddRequest.getParentId());
+
+        return tagDO;
+    }
+
+    public static TagDO convertRequestToDO(TagUpdateRequest tagUpdateRequest, Long userId) {
+        TagDO tagDO = new TagDO();
+
+        tagDO.setId(tagUpdateRequest.getId());
+        tagDO.setUpdateUserId(userId);
+        tagDO.setUpdateTime(new Date());
+
+        tagDO.setCode(tagUpdateRequest.getCode());
+        tagDO.setName(tagUpdateRequest.getName());
+        tagDO.setDescription(tagUpdateRequest.getDescription());
+        tagDO.setParentId(tagUpdateRequest.getParentId());
 
         return tagDO;
     }
