6750ece3b5281c088e6ff74e5db00ada5ad03991
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-07 18:14:43 +0800

    Update AccountingService.java

--- accounting/src/main/java/com/nero/hua/service/AccountingService.java
+++ accounting/src/main/java/com/nero/hua/service/AccountingService.java
@@ -1,9 +1,6 @@
 package com.nero.hua.service;
 
-import com.nero.hua.model.accounting.AccountingAddRequest;
-import com.nero.hua.model.accounting.AccountingPageRequest;
-import com.nero.hua.model.accounting.AccountingResponse;
-import com.nero.hua.model.accounting.AccountingTagListRequest;
+import com.nero.hua.model.accounting.*;
 import com.nero.hua.model.base.BasePageResponse;
 import com.nero.hua.model.tag.TagResponse;
 
@@ -12,6 +9,10 @@ import java.util.List;
 public interface AccountingService {
 
     Long add(AccountingAddRequest accountingAddRequest, String userId);
+
+    Long deleteById(Long id);
+
+    Long updateById(AccountingUpdateRequest accountingUpdateRequest, String userId);
 
     AccountingResponse get(Long id);
 
