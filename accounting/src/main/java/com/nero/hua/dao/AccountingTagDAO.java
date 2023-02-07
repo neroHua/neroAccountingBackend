659177dde68e2500a562dd5887c8e64f3343b1c6
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-07 10:52:44 +0800

    Update AccountingTagDAO.java

--- accounting/src/main/java/com/nero/hua/dao/AccountingTagDAO.java
+++ accounting/src/main/java/com/nero/hua/dao/AccountingTagDAO.java
@@ -3,8 +3,12 @@ package com.nero.hua.dao;
 import com.nero.hua.bean.AccountingTagDO;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface AccountingTagDAO {
     Long insertAccountingTag(@Param("accountingTagDO") AccountingTagDO accountingTagDO);
 
     AccountingTagDO selectById(@Param("id") Long id);
+
+    List<AccountingTagDO> selectListByAccountingId(@Param("accountingId") Long accountingId);
 }
