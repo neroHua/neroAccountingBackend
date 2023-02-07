4808c2961d0e5c1d48de7bf2f3a943bbd82649b7
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-07 16:32:17 +0800

    Update TagDAOTest.java

--- accounting/src/test/java/com/nero/hua/dao/TagDAOTest.java
+++ accounting/src/test/java/com/nero/hua/dao/TagDAOTest.java
@@ -22,17 +22,14 @@ public class TagDAOTest {
     @Test
     public void testInsertUser() {
         TagDO tagDO = new TagDO();
-
         tagDO.setCreateTime(new Date());
         tagDO.setUpdateTime(new Date());
         tagDO.setCreateUserId(0L);
         tagDO.setUpdateUserId(0L);
-
         tagDO.setCode("huadeyouTestCode001");
         tagDO.setName("huadeyouTestName001");
         tagDO.setDescription("huadeyouTestDescription001");
         tagDO.setParentId(1L);
-
         Long id = tagDAO.insertTag(tagDO);
         Assert.assertTrue("插入标签应该成功", id > 0);
 
@@ -44,10 +41,25 @@ public class TagDAOTest {
         Assert.assertEquals("插入前后字段应该相等", tagDO1.getDescription(), tagDO.getDescription());
         Assert.assertEquals("插入前后字段应该相等", tagDO1.getParentId(), tagDO.getParentId());
 
+        tagDO.setUpdateUserId(2L);
+        tagDO.setUpdateTime(new Date());
+        tagDO.setCode("huadeyouTestCode002");
+        tagDO.setName("huadeyouTestName002");
+        tagDO.setDescription("huadeyouTestDescription002");
+        tagDO.setParentId(2L);
+        tagDAO.updateById(tagDO);
+        TagDO tagDO2 = tagDAO.selectById(tagDO.getId());
+        Assert.assertEquals("插入前后字段应该相等", tagDO2.getUpdateUserId(), tagDO.getUpdateUserId());
+        Assert.assertEquals("插入前后字段应该相等", tagDO2.getCode(), tagDO.getCode());
+        Assert.assertEquals("插入前后字段应该相等", tagDO2.getName(), tagDO.getName());
+        Assert.assertEquals("插入前后字段应该相等", tagDO2.getDescription(), tagDO.getDescription());
+        Assert.assertEquals("插入前后字段应该相等", tagDO2.getParentId(), tagDO.getParentId());
+
         tagDAO.deleteById(tagDO.getId());
 
-        TagDO tagDO2 = tagDAO.selectById(tagDO.getId());
-        Assert.assertNull( "应该找不到标签", tagDO2);
+        TagDO tagDO3 = tagDAO.selectById(tagDO.getId());
+        Assert.assertNull( "应该找不到标签", tagDO3);
+
     }
 
     @Test
