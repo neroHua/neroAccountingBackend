4a90bc8ee904cb02799b1048ff827e1624b3fbf5
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-07 10:51:49 +0800

    Update AccountingServiceImpl.java

--- accounting/src/main/java/com/nero/hua/service/impl/AccountingServiceImpl.java
+++ accounting/src/main/java/com/nero/hua/service/impl/AccountingServiceImpl.java
@@ -2,21 +2,27 @@ package com.nero.hua.service.impl;
 
 import com.nero.hua.bean.AccountingDO;
 import com.nero.hua.bean.AccountingTagDO;
+import com.nero.hua.bean.TagDO;
 import com.nero.hua.bean.UserDO;
 import com.nero.hua.convert.AccountingConvert;
 import com.nero.hua.convert.AccountingTagConvert;
+import com.nero.hua.convert.TagConvert;
 import com.nero.hua.dao.AccountingDAO;
 import com.nero.hua.dao.AccountingTagDAO;
+import com.nero.hua.dao.TagDAO;
 import com.nero.hua.dao.UserDAO;
 import com.nero.hua.model.accounting.AccountingAddRequest;
 import com.nero.hua.model.accounting.AccountingPageRequest;
 import com.nero.hua.model.accounting.AccountingResponse;
+import com.nero.hua.model.accounting.AccountingTagListRequest;
 import com.nero.hua.model.base.BasePageResponse;
+import com.nero.hua.model.tag.TagResponse;
 import com.nero.hua.service.AccountingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AccountingServiceImpl implements AccountingService {
@@ -29,6 +35,9 @@ public class AccountingServiceImpl implements AccountingService {
 
     @Autowired
     private UserDAO userDAO;
+
+    @Autowired
+    private TagDAO tagDAO;
 
     @Override
     @Transactional
@@ -64,6 +73,22 @@ public class AccountingServiceImpl implements AccountingService {
         List<AccountingResponse> newsResponseList = AccountingConvert.convertDOToResponse(accountingDOList);
 
         return new BasePageResponse<>(totalCount, newsResponseList);
+    }
+
+    @Override
+    public List<TagResponse> selectAccountingTagList(AccountingTagListRequest accountingTagRequest) {
+        List<AccountingTagDO> accountingTagDOList = accountingTagDAO.selectListByAccountingId(accountingTagRequest.getId());
+
+        if (CollectionUtils.isEmpty(accountingTagDOList)) {
+            return null;
+        }
+
+        List<TagDO> tagDOList = new ArrayList<>();
+        for (AccountingTagDO accountingTagDO : accountingTagDOList) {
+            tagDOList.add(tagDAO.selectById(accountingTagDO.getTagId()));
+        }
+
+        return TagConvert.convertDOToResponse(tagDOList);
     }
 
 }
