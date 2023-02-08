7f9adbb6993f409d1a82ea0e02854b46362b5944
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-08 10:38:49 +0800

    Update AccountingDAOTest.java

--- accounting/src/test/java/com/nero/hua/dao/AccountingDAOTest.java
+++ accounting/src/test/java/com/nero/hua/dao/AccountingDAOTest.java
@@ -1,6 +1,6 @@
 package com.nero.hua.dao;
 
-import com.nero.hua.bean.TagDO;
+import com.nero.hua.bean.AccountingDO;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,57 +17,55 @@ import java.util.Date;
 public class AccountingDAOTest {
 
     @Autowired
-    private TagDAO accountingDAO;
+    private AccountingDAO accountingDAO;
 
     @Test
-    public void testInsertUser() {
-        TagDO tagDO = new TagDO();
-        tagDO.setCreateTime(new Date());
-        tagDO.setUpdateTime(new Date());
-        tagDO.setCreateUserId(0L);
-        tagDO.setUpdateUserId(0L);
-        tagDO.setCode("huadeyouTestCode001");
-        tagDO.setName("huadeyouTestName001");
-        tagDO.setDescription("huadeyouTestDescription001");
-        tagDO.setParentId(1L);
-        Long id = accountingDAO.insertTag(tagDO);
+    public void testCRUD() {
+        AccountingDO accountingDO = new AccountingDO();
+        accountingDO.setCreateTime(new Date());
+        accountingDO.setUpdateTime(new Date());
+        accountingDO.setCreateUserId(0L);
+        accountingDO.setUpdateUserId(0L);
+
+        accountingDO.setUserId(1L);
+
+        accountingDO.setBillMoney(100D);
+        accountingDO.setBillCreateTime(new Date());
+        accountingDO.setDescription("huadeyouTestDescription001");
+        accountingDO.setReviewDescription("huadeyouTestReviewDescription001");
+        accountingDO.setValuable(Boolean.TRUE);
+
+        Long id = accountingDAO.insertAccounting(accountingDO);
         Assert.assertTrue("插入标签应该成功", id > 0);
 
-        TagDO tagDO1 = accountingDAO.selectById(tagDO.getId());
-        Assert.assertEquals("插入前后字段应该相等", tagDO1.getCreateUserId(), tagDO.getCreateUserId());
-        Assert.assertEquals("插入前后字段应该相等", tagDO1.getUpdateUserId(), tagDO.getUpdateUserId());
-        Assert.assertEquals("插入前后字段应该相等", tagDO1.getCode(), tagDO.getCode());
-        Assert.assertEquals("插入前后字段应该相等", tagDO1.getName(), tagDO.getName());
-        Assert.assertEquals("插入前后字段应该相等", tagDO1.getDescription(), tagDO.getDescription());
-        Assert.assertEquals("插入前后字段应该相等", tagDO1.getParentId(), tagDO.getParentId());
+        AccountingDO accountingDO1 = accountingDAO.selectById(accountingDO.getId());
+        testDOEqual(accountingDO, accountingDO1);
 
-        tagDO.setUpdateUserId(2L);
-        tagDO.setUpdateTime(new Date());
-        tagDO.setCode("huadeyouTestCode002");
-        tagDO.setName("huadeyouTestName002");
-        tagDO.setDescription("huadeyouTestDescription002");
-        tagDO.setParentId(2L);
-        accountingDAO.updateById(tagDO);
-        TagDO tagDO2 = accountingDAO.selectById(tagDO.getId());
-        Assert.assertEquals("插入前后字段应该相等", tagDO2.getUpdateUserId(), tagDO.getUpdateUserId());
-        Assert.assertEquals("插入前后字段应该相等", tagDO2.getCode(), tagDO.getCode());
-        Assert.assertEquals("插入前后字段应该相等", tagDO2.getName(), tagDO.getName());
-        Assert.assertEquals("插入前后字段应该相等", tagDO2.getDescription(), tagDO.getDescription());
-        Assert.assertEquals("插入前后字段应该相等", tagDO2.getParentId(), tagDO.getParentId());
+        accountingDO.setUpdateUserId(2L);
+        accountingDO.setUpdateTime(new Date());
+        accountingDO.setBillMoney(1000D);
+        accountingDO.setBillCreateTime(new Date());
+        accountingDO.setDescription("huadeyouTestDescription002");
+        accountingDO.setReviewDescription("huadeyouTestReviewDescription002");
+        accountingDO.setValuable(Boolean.FALSE);
+        accountingDAO.updateById(accountingDO);
+        AccountingDO accountingDO2 = accountingDAO.selectById(accountingDO.getId());
+        testDOEqual(accountingDO, accountingDO2);
 
-        accountingDAO.deleteById(tagDO.getId());
+        accountingDAO.deleteById(accountingDO.getId());
 
-        TagDO tagDO3 = accountingDAO.selectById(tagDO.getId());
-        Assert.assertNull( "应该找不到标签", tagDO3);
+        AccountingDO accountingDO3 = accountingDAO.selectById(accountingDO.getId());
+        Assert.assertNull( "应该找不到标签", accountingDO3);
     }
 
-    @Test
-    public void testSelectById() {
-        TagDO tagDO = accountingDAO.selectById(1L);
-        Assert.assertNotNull( "应该找到标签1", tagDO);
-
-        TagDO tagDO1 = accountingDAO.selectById(-1L);
-        Assert.assertNull( "应该找不到标签-1", tagDO1);
+    private static void testDOEqual(AccountingDO accountingDO, AccountingDO accountingDO1) {
+        Assert.assertEquals("插入前后字段应该相等", accountingDO1.getCreateUserId(), accountingDO.getCreateUserId());
+        Assert.assertEquals("插入前后字段应该相等", accountingDO1.getUpdateUserId(), accountingDO.getUpdateUserId());
+        Assert.assertEquals("插入前后字段应该相等", accountingDO1.getUserId(), accountingDO.getUserId());
+        Assert.assertEquals("插入前后字段应该相等", accountingDO1.getBillMoney(), accountingDO.getBillMoney());
+        Assert.assertEquals("插入前后字段应该相等", accountingDO1.getDescription(), accountingDO.getDescription());
+        Assert.assertEquals("插入前后字段应该相等", accountingDO1.getReviewDescription(), accountingDO.getReviewDescription());
+        Assert.assertEquals("插入前后字段应该相等", accountingDO1.getValuable(), accountingDO.getValuable());
     }
 
 }
