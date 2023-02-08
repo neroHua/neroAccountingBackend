1384b6b5c08efedb33e706c90cbac07f1bc310dd
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-08 11:28:32 +0800

    Update AccountingTagConvert.java

--- accounting/src/main/java/com/nero/hua/convert/AccountingTagConvert.java
+++ accounting/src/main/java/com/nero/hua/convert/AccountingTagConvert.java
@@ -1,10 +1,15 @@
 package com.nero.hua.convert;
 
 import com.nero.hua.bean.AccountingTagDO;
+import com.nero.hua.model.accounting.AccountingTagAddRequest;
 
 import java.util.Date;
 
 public class AccountingTagConvert {
+
+    public static AccountingTagDO convertRequestToDO(AccountingTagAddRequest accountingTagAddRequest, Long userId) {
+        return convertDO(userId, accountingTagAddRequest.getAccountingId(), accountingTagAddRequest.getTagId());
+    }
 
     public static AccountingTagDO convertDO(Long userId, long accountingDOId, long tagId) {
         AccountingTagDO accountingTagDO = new AccountingTagDO();
