4c75c095ca5e254efd560d3f32530cd00c750fb6
neroHua <38644203+neroHua@users.noreply.github.com>
2023-02-24 10:59:29 +0800

    Update BasePageRequest.java

--- accounting/src/main/java/com/nero/hua/model/base/BasePageRequest.java
+++ accounting/src/main/java/com/nero/hua/model/base/BasePageRequest.java
@@ -3,12 +3,18 @@ package com.nero.hua.model.base;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Setter
 @Getter
 public class BasePageRequest {
 
+    @NotNull(message = "每页数量不能为null")
+    @Min(value = 0, message = "每页数量不能小于0")
     private Integer pageSize;
-
+    @NotNull(message = "页码不能为null")
+    @Min(value = 0, message = "页码不能小于0")
     private Integer pageNumber;
 
     private String keyword;
