510a540a8f987525cd501d5e5386a38e253d8763
neroHua <38644203+neroHua@users.noreply.github.com>
2023-03-03 15:37:47 +0800

    Update StatisticsAndAnalysisService.java

--- accounting/src/main/java/com/nero/hua/service/StatisticsAndAnalysisService.java
+++ accounting/src/main/java/com/nero/hua/service/StatisticsAndAnalysisService.java
@@ -2,6 +2,8 @@ package com.nero.hua.service;
 
 import com.nero.hua.model.statisticsAndAnalysis.TotalMoneyForEveryMonthRequest;
 import com.nero.hua.model.statisticsAndAnalysis.TotalMoneyForEveryMonthResponse;
+import com.nero.hua.model.statisticsAndAnalysis.TotalMoneyForEverydayRequest;
+import com.nero.hua.model.statisticsAndAnalysis.TotalMoneyForEverydayResponse;
 
 import java.util.List;
 
@@ -9,4 +11,5 @@ public interface StatisticsAndAnalysisService {
 
     List<TotalMoneyForEveryMonthResponse> getTotalMoneyForEveryMonth(TotalMoneyForEveryMonthRequest totalMoneyForEveryMonthRequest, String userId);
 
+    List<TotalMoneyForEverydayResponse> getTotalMoneyForEveryday(TotalMoneyForEverydayRequest totalMoneyForEverydayRequest, String userId);
 }
