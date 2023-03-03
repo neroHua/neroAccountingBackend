0fd219d764e429f30cb6698e5e52d5328d26a610
neroHua <38644203+neroHua@users.noreply.github.com>
2023-03-03 14:09:04 +0800

    Create StatisticsAndAnalysisController.java

---
+++ accounting/src/main/java/com/nero/hua/controller/StatisticsAndAnalysisController.java
@@ -0,0 +1,32 @@
+package com.nero.hua.controller;
+
+import com.nero.hua.model.base.BaseResponse;
+import com.nero.hua.model.statisticsAndAnalysis.TotalMoneyForEveryMonthRequest;
+import com.nero.hua.model.statisticsAndAnalysis.TotalMoneyForEveryMonthResponse;
+import com.nero.hua.service.StatisticsAndAnalysisService;
+import com.nero.hua.util.LoginUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping("/statisticsAndAnalysis")
+public class StatisticsAndAnalysisController {
+
+    @Autowired
+    StatisticsAndAnalysisService statisticsAndAnalysisService;
+
+    @GetMapping("/totalMoneyForEveryMonth")
+    public BaseResponse<List<TotalMoneyForEveryMonthResponse>> getTotalMoneyForEveryMonth(@Validated TotalMoneyForEveryMonthRequest totalMoneyForEveryMonthRequest, HttpServletRequest httpServletRequest) {
+        String userId = LoginUtil.parseUserIdFromHttpServletRequest(httpServletRequest);
+        List<TotalMoneyForEveryMonthResponse> totalMoneyForEveryMonthResponseList = statisticsAndAnalysisService.getTotalMoneyForEveryMonth(totalMoneyForEveryMonthRequest, userId);
+
+        return new BaseResponse<>(totalMoneyForEveryMonthResponseList);
+    }
+
+}
