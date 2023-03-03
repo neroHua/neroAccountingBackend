68b4a1a37a4317b2d9004699226e49b769e13f75
neroHua <38644203+neroHua@users.noreply.github.com>
2023-03-03 15:39:15 +0800

    Create TotalMoneyForEverydayResponse.java

---
+++ accounting/src/main/java/com/nero/hua/model/statisticsAndAnalysis/TotalMoneyForEverydayResponse.java
@@ -0,0 +1,14 @@
+package com.nero.hua.model.statisticsAndAnalysis;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class TotalMoneyForEverydayResponse {
+
+    Double money;
+
+    String day;
+
+}
