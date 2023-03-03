474875ba98f3978cfb1b0feff901a796e5708648
neroHua <38644203+neroHua@users.noreply.github.com>
2023-03-03 15:39:02 +0800

    Create TotalMoneyForEverydayRequest.java

---
+++ accounting/src/main/java/com/nero/hua/model/statisticsAndAnalysis/TotalMoneyForEverydayRequest.java
@@ -0,0 +1,22 @@
+package com.nero.hua.model.statisticsAndAnalysis;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+public class TotalMoneyForEverydayRequest {
+
+    private Date billCreateTimeMin;
+
+    private Date billCreateTimeMax;
+
+    private Double billMoneyMin;
+
+    private Double billMoneyMax;
+
+    private Boolean valuable;
+
+}
