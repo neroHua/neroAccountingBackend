eee6127dad954bdd651228f19a14e9965888c0b8
neroHua <38644203+neroHua@users.noreply.github.com>
2023-03-03 15:38:03 +0800

    Update StatisticsAndAnalysisServiceImpl.java

--- accounting/src/main/java/com/nero/hua/service/impl/StatisticsAndAnalysisServiceImpl.java
+++ accounting/src/main/java/com/nero/hua/service/impl/StatisticsAndAnalysisServiceImpl.java
@@ -3,6 +3,8 @@ package com.nero.hua.service.impl;
 import com.nero.hua.dao.AccountingDAO;
 import com.nero.hua.model.statisticsAndAnalysis.TotalMoneyForEveryMonthRequest;
 import com.nero.hua.model.statisticsAndAnalysis.TotalMoneyForEveryMonthResponse;
+import com.nero.hua.model.statisticsAndAnalysis.TotalMoneyForEverydayRequest;
+import com.nero.hua.model.statisticsAndAnalysis.TotalMoneyForEverydayResponse;
 import com.nero.hua.service.StatisticsAndAnalysisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,11 @@ public class StatisticsAndAnalysisServiceImpl implements StatisticsAndAnalysisSe
     @Override
     public List<TotalMoneyForEveryMonthResponse> getTotalMoneyForEveryMonth(TotalMoneyForEveryMonthRequest totalMoneyForEveryMonthRequest, String userId) {
         return accountingDAO.getTotalMoneyForEveryMonth(totalMoneyForEveryMonthRequest);
+    }
+
+    @Override
+    public List<TotalMoneyForEverydayResponse> getTotalMoneyForEveryday(TotalMoneyForEverydayRequest totalMoneyForEverydayRequest, String userId) {
+        return accountingDAO.getTotalMoneyForEveryday(totalMoneyForEverydayRequest);
     }
 
 }
